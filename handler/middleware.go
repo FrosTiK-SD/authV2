a47bd3ca75ae03d91a6ad79d8f175f1aca647482
Shubhrajyoti Dey <75834423+Shubhrajyoti-Dey-FrosTiK@users.noreply.github.com>
2024-01-28 22:33:26 +0530

    AuthV2 Gin Middleware + AuthV2 Package 1.0 (#20)
    * AuthV2 middleware support for GIN
    
    * Added constatnts

---
+++ handler/middleware.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"github.com/FrosTiK-SD/authV2/constants"
+	"github.com/gin-gonic/gin"
+)
+
+// For Gin based middlewares
+func (h *Handler) GinVerifyStudent(ctx *gin.Context) {
+
+	// Create a new session
+	currentHandler := Handler{
+		MongikClient: h.MongikClient,
+		JwkSet:       h.JwkSet,
+		Session:      &Session{},
+		Config: Config{
+			Mode: MIDDLEWARE,
+		},
+	}
+
+	currentHandler.HandlerVerifyStudentIdToken(ctx)
+	student := currentHandler.Session.Student
+
+	if student != nil {
+		ctx.Set(constants.SESSION_STUDENT, student)
+		ctx.Next()
+	}
+}
