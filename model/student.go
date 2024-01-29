4ff40b18b0f81f9eb9cf83c005a4fb4fca88e72a
Shubhrajyoti Dey <toshubhrajyotidey@gmail.com>
2024-01-29 22:00:06 +0530

    Replaced groups

--- model/student.go
+++ model/student.go
@@ -1,17 +1,11 @@
 package model
 
 import (
+	group "github.com/FrosTiK-SD/models/company"
 	studentModel "github.com/FrosTiK-SD/models/student"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
-
-type Group struct {
-	ID    primitive.ObjectID `json:"_id" bson:"_id"`
-	Name  string             `json:"name" bson:"name"`
-	Roles []string           `json:"roles" bson:"roles"`
-}
 
 type StudentPopulated struct {
 	studentModel.Student
-	GroupDetails []Group `json:"groups" bson:"groups"`
+	GroupDetails []group.Group `json:"groups" bson:"groups"`
 }
