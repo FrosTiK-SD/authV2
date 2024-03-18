0651379f262c49fb34253040aa7ea5b2314dd556
Soumik Dutta <shalearkane@gmail.com>
2024-03-19 01:09:24 +0530

    add endpoint to register student

--- handler/student.go
+++ handler/student.go
@@ -3,10 +3,13 @@ package handler
 import (
 	"github.com/FrosTiK-SD/auth/constants"
 	"github.com/FrosTiK-SD/auth/controller"
+	"github.com/FrosTiK-SD/auth/interfaces"
+	"github.com/FrosTiK-SD/models/constant"
 	studentModel "github.com/FrosTiK-SD/models/student"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func (h *Handler) HandlerUpdateStudentDetails(ctx *gin.Context) {
@@ -33,6 +36,44 @@ func (h *Handler) HandlerUpdateStudentDetails(ctx *gin.Context) {
 		return
 	} else {
 		ctx.JSON(200, gin.H{"student": updateResult})
+	}
+}
 
+func (h *Handler) HandlerRegisterStudentDetails(ctx *gin.Context) {
+	idToken := ctx.GetHeader("token")
+	studentCollection := h.MongikClient.MongoClient.Database(constants.DB).Collection(constants.COLLECTION_STUDENT)
+	newStudentDetails := interfaces.StudentRegistration{}
+
+	if errBinding := ctx.ShouldBindBodyWith(&newStudentDetails, binding.JSON); errBinding != nil {
+		ctx.AbortWithStatusJSON(401, gin.H{"error": errBinding.Error()})
+		return
+	}
+
+	if email, _, errVerify := controller.VerifyToken(h.MongikClient.CacheClient, idToken, h.JwkSet, true); errVerify != nil {
+		ctx.AbortWithStatusJSON(401, gin.H{"error": errVerify})
+		return
+	} else if email != &newStudentDetails.InstituteEmail {
+		ctx.AbortWithStatusJSON(401, gin.H{"error": "Email mismatch"})
+	}
+
+	newStudent := studentModel.Student{
+		Id:             primitive.NewObjectID(),
+		Batch:          &newStudentDetails.Batch,
+		RollNo:         newStudentDetails.RollNo,
+		InstituteEmail: newStudentDetails.InstituteEmail,
+		Department:     newStudentDetails.Department,
+		Course:         (*constant.Course)(&newStudentDetails.Course),
+		Specialisation: newStudentDetails.Specialisation,
+		FirstName:      newStudentDetails.FirstName,
+		MiddleName:     newStudentDetails.MiddleName,
+		LastName:       newStudentDetails.LastName,
+	}
+
+	if result, err := studentCollection.InsertOne(ctx, newStudent); err != nil {
+		ctx.AbortWithStatusJSON(401, gin.H{"error": err.Error()})
+		return
+	} else {
+		ctx.JSON(200, gin.H{"student": newStudent, "logs": result})
+		return
 	}
 }
--- main.go
+++ main.go
@@ -49,6 +49,7 @@ func main() {
 
 	r.GET("/api/token/student/verify", handler.HandlerVerifyStudentIdToken)
 	r.POST("/api/student/update", handler.GinVerifyStudent, handler.HandlerUpdateStudentDetails)
+	r.POST("/api/student/register", handler.HandlerRegisterStudentDetails)
 	r.GET("/api/token/invalidate_cache", handler.InvalidateCache)
 
 	port := "" + os.Getenv("PORT")
