0a8571f539fce8776aa257a34f22cc1e7c015ff3
Soumik Dutta <shalearkane@gmail.com>
2024-04-15 21:53:50 +0530

    add very basic code to get profile, mapping value to profile still remains

---
+++ controller/profile.go
@@ -0,0 +1,12 @@
+package controller
+
+import (
+	"github.com/FrosTiK-SD/auth/interfaces"
+	"github.com/FrosTiK-SD/auth/model"
+)
+
+func MapStudentToStudentProfile(student *model.StudentPopulated) interfaces.StudentProfile {
+	var profile interfaces.StudentProfile
+
+	return profile
+}
